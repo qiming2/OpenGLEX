61dc216bdef567039b5fccee1cf8289f012481d0
qiming2 <pguan2017@gmail.com>
2020-10-03 02:40:34 -0700

    Added glGenbuffers

--- src/App.cpp
+++ src/App.cpp
@@ -27,39 +27,14 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    // Creating buffer
+    glGenBuffers(1, &buffer);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0.0f, 1.0f, 0.0f); // Green
-        glVertex2f(0.0f, 0.0f);
-        glColor3f(0.0f, 0.0f, 1.0f); // Blue
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-        //glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-        //glColor3f(1.0f, 0.0f, 0.0f); // Red
-        //glVertex2f(-0.5f, -0.5f);    // x, y
-        //glVertex2f(0.5f, -0.5f);
-        //glVertex2f(0.5f, 0.5f);
-        //glVertex2f(-0.5f, 0.5f);
-        //glEnd();
-        glBegin(GL_TRIANGLES);          // Each set of 3 vertices form a triangle
-        //glColor3f(0.0f, 0.0f, 1.0f); // Blue
-        glVertex2f(0.1f, -0.6f);
-        glVertex2f(0.7f, -0.6f);
-        glVertex2f(0.4f, -0.1f);
-
-        glColor3f(1.0f, 0.0f, 0.0f); // Red
-        glVertex2f(0.3f, -0.4f);
-        glColor3f(0.0f, 1.0f, 0.0f); // Green
-        glVertex2f(0.9f, -0.4f);
-        glColor3f(0.0f, 0.0f, 1.0f); // Blue
-        glVertex2f(0.6f, -0.9f);
-        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
