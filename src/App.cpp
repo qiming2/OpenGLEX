0531009feabfe659b99595065ecf75c6caa22335
qiming2 <pguan2017@gmail.com>
2020-10-03 03:18:57 -0700

    Finished writing the first shader

--- src/App.cpp
+++ src/App.cpp
@@ -8,8 +8,52 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
     unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
     // id: the shader whose source code would be replaced
-    // 
+    // size: how many number of strings in the array of strings
+    // the pointer to the array of strings
+    // pointer to an array of integers specifying the length of each string
+    //      nullptr means that each string is terminated with null
     glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    // TODO: Error handling
+    int result;
+    // A way to get status for a specific operation that has been done with opengl
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    
+    if (result == GL_FALSE) {
+        int length;
+        // getting message from opengl
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length *sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!"
+            << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    // Attach shader objects to the program
+    // Then we link them together
+    // For details, read the documentation: docs.GL
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 
 int main(void)
@@ -58,6 +102,33 @@ int main(void)
 
     // Needs to enable the vertex attri
     glEnableVertexAttribArray(0);
+
+    // Write shaders
+
+    // Better to write in a file and load them into c++ string
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+
+    // bind shader or select the shader to use
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
