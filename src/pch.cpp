399e749d34df305885ef9296f449612e4d44fdb6
qiming2 <pguan2017@gmail.com>
2021-07-29 01:56:18 -0700

    delete pch

--- src/pch.cpp
+++
@@ -1,5 +0,0 @@
-#include "pch.h"
-
-// Not in use since needing to add to all source 
-// files but just be deemed a reminder that precompiled headers can be very
-// important when making a considerably big projects
