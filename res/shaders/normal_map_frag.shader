6bf7714c889823f9a44a0a603423c0e0b2c4c80c
qiming2 <pguan2017@gmail.com>
2021-10-09 11:12:02 -0700

    Finished steep parallax mapping and occlusion mapping

--- res/shaders/normal_map_frag.shader
+++ res/shaders/normal_map_frag.shader
@@ -25,19 +25,52 @@ uniform float heightScale;
 vec2 textureCoordWithHeightMap();
 
 vec2 textureCoordWithHeightMap(vec3 TBNViewDir) {
-	// get height value off the height map and then
+	// ----------------------------------- essential method --------------------
+	// get depth value off the depth map and then
 	// scale normalized view vector by the height value
 	float height = texture(texture_height1, vs.UV).r;
 	// dividing by z is to make p larger when looking at a steep angle
 	// than when TBNViewDir is perpendicular to the surface
 	vec2 p = TBNViewDir.xy / TBNViewDir.z * (height * heightScale);
-	return vs.UV - p;
+
+	//return vs.UV - p;
+	// ------------------------------------- Sampling method for steep parallax mapping ----
+	// would still have artifacts since only several samples are taken
+	float maxLayer = 32.0;
+	float minLayer = 8.0;
+	// Samples taken depend on angle of TBNViewDir
+	float numLayer = mix(maxLayer, minLayer, 1.0 - max(dot(TBNViewDir, vec3(0.0, 0.0, 1.0)), 0.0));
+
+	float layerVal = 1.0 / numLayer;
+	float curLayer = 0.0;
+	vec2 layVec = TBNViewDir.xy / TBNViewDir.z  / numLayer * (height * heightScale);
+	vec2 curTex = vs.UV;
+	float depthVal = texture(texture_height1, vs.UV).r;
+	
+	while (curLayer < depthVal && numLayer > 0.0) {
+		curLayer += layerVal;
+		curTex -= layVec;
+		numLayer -= 1.0;
+		depthVal = texture(texture_height1, curTex).r;
+	}
+
+	
+	// --------------------------------- Parallax Occlusion Mapping -----------------------
+	// interpolate between the layer after hitting a depth val that is less than curLayer
+	// and the one before it
+	float afterDepthDiff = curLayer - depthVal;
+	vec2 preTexCoord = curTex + layVec;
+	float beforeDepthDiff = texture(texture_height1, preTexCoord).r - curLayer + layerVal;
+
+	float weight = afterDepthDiff / (afterDepthDiff + beforeDepthDiff);
+	vec2 texC = weight * preTexCoord + (1.0 - weight) * curTex;
+	return texC;
 }
 
 void main() {
 	light.intensity = vec3(1.0, 1.0, 1.0);
-	light.dir = -normalize(vec3(0.0, 1.0, 1.0));
-	vec3 viewDir = viewPos - vs.Pos;
+	light.dir = normalize(vec3(1.0, 1.0, -1.0));
+	vec3 viewDir = normalize(viewPos - vs.Pos);
 	vec2 texCoord;
 	if (parallexMapping) {
 		vec3 TBNViewDir = normalize(transpose(vs.TBN) * viewDir);
@@ -54,7 +87,7 @@ void main() {
 	// transform sampled normal from tangent space to world space
 	// since it is from 0 to 1, we want to map them back to -1 to 1
 	vec3 normal = texture(texture_normal1, texCoord).rgb;
-	normal = normalize(vs.TBN * (normal * 2.0 - 1.0));
+	normal = normalize(vs.TBN * normalize(normal * 2.0 - 1.0));
 	if (setNorm) {
 		normal = vs.Normal;
 	}
--- src/Scene/NormalMapScene.cpp
+++ src/Scene/NormalMapScene.cpp
@@ -3,25 +3,17 @@
 #include "Mesh.h"
 
 namespace Scene {
-	static bool parallexMapping = 0;
+	static bool parallexMapping = true;
 	static float heightScale = 0.1f;
 	static bool hardCode = false;
 	static bool normalized = false;
 	static glm::vec3 bi;
 	static glm::vec3 ta;
-	unsigned int quadVAO = 0;
-	unsigned int quadVBO;
-	void static renderQuad()
-	{
-		glBindVertexArray(quadVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindVertexArray(0);
-	}
 
 NormalMapScene::NormalMapScene():
-	diffuseMap("res/Texture/bricks2.jpg", 0),
-	normalMap("res/Texture/bricks2_normal.jpg", 1),
-	heightMap("res/Texture/bricks2_disp.jpg", 2),
+	diffuseMap("res/Texture/wood.png", 0),
+	normalMap("res/Texture/toy_box_normal.png", 1),
+	heightMap("res/Texture/toy_box_disp.png", 2),
 	shader("res/shaders/normal_map_vert.shader", "res/shaders/normal_map_frag.shader")
 {
 	std::vector<unsigned int> indices = { 0, 1, 2, 3, 4, 5 };
@@ -29,101 +21,6 @@ NormalMapScene::NormalMapScene():
 	std::vector<Vertex> vertices;
 	vertices.reserve(plane.size() / 8);
 	{
-
-		// ----------------------------------Tutorial-----------------------------
-		if (quadVAO == 0)
-		{
-			// positions
-			glm::vec3 pos1(-1.0f, 1.0f, 0.0f);
-			glm::vec3 pos2(-1.0f, -1.0f, 0.0f);
-			glm::vec3 pos3(1.0f, -1.0f, 0.0f);
-			glm::vec3 pos4(1.0f, 1.0f, 0.0f);
-			// texture coordinates
-			glm::vec2 uv1(0.0f, 1.0f);
-			glm::vec2 uv2(0.0f, 0.0f);
-			glm::vec2 uv3(1.0f, 0.0f);
-			glm::vec2 uv4(1.0f, 1.0f);
-			// normal vector
-			glm::vec3 nm(0.0f, 0.0f, 1.0f);
-
-			// calculate tangent/bitangent vectors of both triangles
-			glm::vec3 tangent1, bitangent1;
-			glm::vec3 tangent2, bitangent2;
-			// triangle 1
-			// ----------
-			glm::vec3 edge1 = pos2 - pos1;
-			glm::vec3 edge2 = pos3 - pos1;
-			glm::vec2 deltaUV1 = uv2 - uv1;
-			glm::vec2 deltaUV2 = uv3 - uv1;
-
-			float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-			tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-			tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-			tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-
-			bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-			bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-			bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-			
-			// triangle 2
-			// ----------
-			edge1 = pos3 - pos1;
-			edge2 = pos4 - pos1;
-			deltaUV1 = uv3 - uv1;
-			deltaUV2 = uv4 - uv1;
-
-			f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-			tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-			tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-			tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-
-
-			bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-			bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-			bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-
-
-			/*glm::vec3 _tangent1 = glm::mat3(model) * tangent1;
-			glm::vec3 _bitangent1 = glm::mat3(model) * bitangent1;
-			glm::vec3 _tangent2 = glm::mat3(model) * tangent1;
-			glm::vec3 _bitangent2 = glm::mat3(model) * bitangent1;*/
-			/*std::cout << "Tutorial: T1: " << _tangent1 << "Tutorial: T2: " << _tangent2 << "Tutorial: B1: " << _bitangent1 << "Tutorial: B2: " << _bitangent2 << std::endl;*/
-			float quadVertices[] = {
-				// positions            // normal         // texcoords  // tangent                          // bitangent
-				pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-				pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-				pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-
-				pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-				pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-				pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
-			};
-
-			std::cout << sizeof(quadVertices) << std::endl;
-			// configure plane VAO
-			glGenVertexArrays(1, &quadVAO);
-			glGenBuffers(1, &quadVBO);
-			glBindVertexArray(quadVAO);
-			glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-			glEnableVertexAttribArray(0);
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
-			glEnableVertexAttribArray(1);
-			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
-			glEnableVertexAttribArray(2);
-			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
-			glEnableVertexAttribArray(3);
-			glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
-			glEnableVertexAttribArray(4);
-			glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
-		}
-
-		// ------------------------------------Tutorial above-----------
 		Vertex cur;
 
 		// we first gather all vertices into Vertex struct
@@ -203,7 +100,7 @@ NormalMapScene::NormalMapScene():
 	glVertexAttribPointer(3, 3, GL_FLOAT, false, sizeof(Vertex), (const void*)(8 * sizeof(float)));
 	glEnableVertexAttribArray(4);
 	glVertexAttribPointer(4, 3, GL_FLOAT, false, sizeof(Vertex), (const void*)(11 * sizeof(float)));
-	std::cout << sizeof(Vertex) << std::endl;
+	//std::cout << sizeof(Vertex) << std::endl;
 
 	glBindVertexArray(0);
 }
@@ -219,38 +116,7 @@ NormalMapScene::~NormalMapScene() {
 }
 
 void NormalMapScene::OnRendering() {
-	model = glm::mat4(1.0f);
-	//model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	gl_renderer.Clear();
-	GLCall(glBindVertexArray(vao));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
-	shader.Bind();
-	camera.SetViewProjectMat(shader);
-	shader.SetInt("tuto", 1);
-	glActiveTexture(GL_TEXTURE0 + 0);
-	diffuseMap.Bind();
-	glActiveTexture(GL_TEXTURE0 + 1);
-	normalMap.Bind();
-	glActiveTexture(GL_TEXTURE0 + 2);
-	heightMap.Bind();
-	shader.SetInt("setNorm", 0);
-	shader.SetInt("parallexMapping", parallexMapping);
-	shader.SetMat4fv("model", model);
-	shader.SetVec3fv("viewPos", camera.pos);
-	shader.SetFloat("heightScale", heightScale);
-	//GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-	renderQuad();
-	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-	//model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	shader.SetMat4fv("model", model);
-	shader.SetInt("parallexMapping", 0);
-	shader.SetInt("setNorm", 1);
-	//GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-	renderQuad();
-	glBindVertexArray(0);
-	// ----------------------------------- Tutorial vs Mine ---------------------------
 	model = glm::mat4(1.0f);
 	model = glm::translate(model, glm::vec3(-2.0f, 0.0f, 0.0f));
 	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
